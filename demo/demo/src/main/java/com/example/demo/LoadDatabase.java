6c93ec2e9360bc6d4825d3b4511138a02f307201
Louis Sullivan <sullivanlouis0@gmail.com>
2023-06-12 21:18:43 +0100

    Account repo interface and the initDatabase functionality

---
+++ demo/demo/src/main/java/com/example/demo/LoadDatabase.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.Locale;
+
+@Configuration
+class LoadDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(AccountRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Account("louis@gmail.com", "Louis", "Sullvian", "lsullivan1", new Locale("en", "IE"), LocalDate.of(2001, 5, 16))));
+        };
+    }
+}

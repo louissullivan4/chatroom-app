457c561155023d976acbd19106f22dd19486b175
Louis Sullivan <sullivanlouis0@gmail.com>
2023-06-13 17:18:36 +0100

    added update, create and delete Account

--- demo/demo/src/main/java/com/example/demo/AppController.java
+++ demo/demo/src/main/java/com/example/demo/AppController.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 import com.example.demo.errors.server.AccountNotFoundException;
 import com.example.demo.errors.server.RoomNotFoundException;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 class AppController {
     private final AccountRepository accountRepository;
@@ -18,19 +22,53 @@ class AppController {
     }
 
     // Get all accounts
+    // curl localhost:8080/accounts
     @GetMapping("/accounts")
     List<Account> allAccounts() {
         return accountRepository.findAll();
     }
 
     // Get one account by ID
+    // curl localhost:8080/accounts/1
+    // curl localhost:8080/accounts/2
     @GetMapping("/accounts/{id}")
     Account oneAccount(@PathVariable Long id) {
         return accountRepository.findById(id)
                 .orElseThrow(() -> new AccountNotFoundException(id));
     }
 
-        // Get all accounts
+    // Create a new account
+    // curl -H "Content-Type: application/json" -X POST http://localhost:8080/accounts -d "{\"email\":\"amy@gmail.com\", \"firstName\":\"Amy\", \"lastName\":\"Murphy\", \"username\":\"amurphy1\", \"country\":\"en_IE\", \"dob\":\"1980_1_1\"}"
+    @PostMapping("/accounts")
+    Account newAccount(@RequestBody Map<Object, String> request) {
+        Account account = new Account();
+        account.newAccountDetails(request);
+        accountRepository.save(account);
+        return account;
+    }
+
+    // Update an account
+    // curl -H "Content-Type: application/json" -X PUT http://localhost:8080/accounts/2 -d "{\"email\":\"dylan@gmail.com\", \"firstName\":\"Bob\", \"lastName\":\"Dylan\", \"username\":\"bdylan1\", \"country\":\"en_IE\", \"dob\":\"1960_1_1\"}"
+    @PutMapping("/accounts/{id}")
+    Account updateAccount(@RequestBody Map<Object, String> request, @PathVariable Long id) throws AccountNotFoundException {
+        if (accountRepository.findById(id).isEmpty()) {
+            throw new AccountNotFoundException(id);
+        } else {
+            Account account = accountRepository.findById(id).get();
+            account.newAccountDetails(request);
+            accountRepository.save(account);
+            return account;
+        }
+    }
+    // Delete an account
+    // curl -X DELETE http://localhost:8080/accounts/4
+    @DeleteMapping("/accounts/{id}")
+    void deleteAccount(@PathVariable Long id) {
+        accountRepository.deleteById(id);
+
+    }
+
+    // Get all accounts
     @GetMapping("/rooms")
     List<Room> allRooms() {
         return roomRepository.findAll();
@@ -42,6 +80,4 @@ class AppController {
         return roomRepository.findById(id)
                 .orElseThrow(() -> new RoomNotFoundException(id));
     }
-
-
 }

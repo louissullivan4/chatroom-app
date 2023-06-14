e2100be218e0a4fe2cee4231ad3c1547bca47945
Louis Sullivan <sullivanlouis0@gmail.com>
2023-06-14 19:05:22 +0100

    added tests for account create, delete and update

--- demo/demo/src/test/java/com/example/demo/AppControllerTests.java
+++ demo/demo/src/test/java/com/example/demo/AppControllerTests.java
@@ -65,4 +65,39 @@ public class AppControllerTests {
         when(accountRepository.findById(1L)).thenReturn(Optional.empty());
         this.mockMvc.perform(MockMvcRequestBuilders.get("/accounts/1")).andDo(print()).andExpect(status().is4xxClientError());
     }
+
+    @Test
+    public void newAccountShouldCreateNewAccount() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/accounts").contentType("application/json").content("{\"email\":\"amy@gmail.com\", \"firstName\":\"Amy\", \"lastName\":\"Murphy\", \"username\":\"amurphy1\", \"country\":\"en_IE\", \"dob\":\"1980_1_1\"}"
+                .getBytes()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("amurphy1"));
+    }
+
+    @Test
+    public void updateAccountShouldChangeSavedAccount() throws Exception {
+        when(accountRepository.findById(2L)).thenReturn(Optional.of(new Account("bob@gmail.com", "Bob", "Dylan", "bdylan1", new Locale("en", "IE"), LocalDate.of(1960, 1, 1))));
+        this.mockMvc.perform(MockMvcRequestBuilders.put("/accounts/2").contentType("application/json").content("{\"email\":\"bob@gmail.com\", \"firstName\":\"Bob\", \"lastName\":\"Dylan\", \"username\":\"dylan1bob\", \"country\":\"en_IE\", \"dob\":\"1960_1_1\"}"
+                .getBytes()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("dylan1bob"));
+
+    }
+
+    @Test
+    public void updateAccountShouldErrorIfNoMatch() throws Exception {
+        when(accountRepository.findById(2L)).thenReturn(Optional.empty());
+        this.mockMvc.perform(MockMvcRequestBuilders.put("/accounts/2").contentType("application/json").content("{\"email\":\"bob@gmail.com\", \"firstName\":\"Bob\", \"lastName\":\"Dylan\", \"username\":\"dylan1bob\", \"country\":\"en_IE\", \"dob\":\"1960_1_1\"}"
+                .getBytes()))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void deleteAccountShouldRemoveAccount() throws Exception {
+        when(accountRepository.findById(2L)).thenReturn(Optional.of(new Account("bob@gmail.com", "Bob", "Dylan", "bdylan1", new Locale("en", "IE"), LocalDate.of(1960, 1, 1))));
+        this.mockMvc.perform(MockMvcRequestBuilders.delete("/accounts/2")).andDo(print()).andExpect(status().isOk());
+    }
 }

e5592eae44bb68697bdb1362b7387864deb9cb31
Louis Sullivan <sullivanlouis0@gmail.com>
2023-06-14 18:51:13 +0100

    clean up app controller

--- demo/demo/src/main/java/com/example/demo/AppController.java
+++ demo/demo/src/main/java/com/example/demo/AppController.java
@@ -1,11 +1,7 @@
 package com.example.demo;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 import com.example.demo.errors.server.AccountNotFoundException;
 import com.example.demo.errors.server.RoomNotFoundException;
@@ -22,15 +18,12 @@ class AppController {
     }
 
     // Get all accounts
-    // curl localhost:8080/accounts
     @GetMapping("/accounts")
     List<Account> allAccounts() {
         return accountRepository.findAll();
     }
 
     // Get one account by ID
-    // curl localhost:8080/accounts/1
-    // curl localhost:8080/accounts/2
     @GetMapping("/accounts/{id}")
     Account oneAccount(@PathVariable Long id) {
         return accountRepository.findById(id)
@@ -38,7 +31,6 @@ class AppController {
     }
 
     // Create a new account
-    // curl -H "Content-Type: application/json" -X POST http://localhost:8080/accounts -d "{\"email\":\"amy@gmail.com\", \"firstName\":\"Amy\", \"lastName\":\"Murphy\", \"username\":\"amurphy1\", \"country\":\"en_IE\", \"dob\":\"1980_1_1\"}"
     @PostMapping("/accounts")
     Account newAccount(@RequestBody Map<Object, String> request) {
         Account account = new Account();
@@ -48,7 +40,6 @@ class AppController {
     }
 
     // Update an account
-    // curl -H "Content-Type: application/json" -X PUT http://localhost:8080/accounts/2 -d "{\"email\":\"dylan@gmail.com\", \"firstName\":\"Bob\", \"lastName\":\"Dylan\", \"username\":\"bdylan1\", \"country\":\"en_IE\", \"dob\":\"1960_1_1\"}"
     @PutMapping("/accounts/{id}")
     Account updateAccount(@RequestBody Map<Object, String> request, @PathVariable Long id) throws AccountNotFoundException {
         if (accountRepository.findById(id).isEmpty()) {
@@ -61,20 +52,18 @@ class AppController {
         }
     }
     // Delete an account
-    // curl -X DELETE http://localhost:8080/accounts/4
     @DeleteMapping("/accounts/{id}")
     void deleteAccount(@PathVariable Long id) {
         accountRepository.deleteById(id);
-
     }
 
-    // Get all accounts
+    // Get all rooms
     @GetMapping("/rooms")
     List<Room> allRooms() {
         return roomRepository.findAll();
     }
 
-    // Get one account by ID
+    // Get one room by ID
     @GetMapping("/rooms/{id}")
     Room oneRoom(@PathVariable Long id) {
         return roomRepository.findById(id)

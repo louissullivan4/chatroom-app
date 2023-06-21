5575cfcb36d29eeeb1a496d9bd84438430675e66
CianMcDonald <cianmcdonald2001@gmail.com>
2023-06-21 16:06:05 +0100

    Added TestUtil class

---
+++ demo/demo/src/test/java/com/example/demo/TestUtil.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class TestUtil {
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            return mapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
